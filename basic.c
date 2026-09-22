7a3d7d318ede3b7f03bef79d3a1bc3ed453a7dd9
lukeknight16112006-1611 <lukeknight16112006@gmail.com>
2026-09-22 21:41:52 +0530

    Basic c code

---
+++ basic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+    float height = 9.889;
+    char initial = 'A';
+    char name[] = "Vickey";
+
+    printf("%d\n", age);
+    printf("%.3f\n", height);
+    printf("%c\n", initial);
+    printf("%s\n", name);
+
+    return 0;
+}
