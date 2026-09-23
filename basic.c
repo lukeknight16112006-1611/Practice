26837fe4404ebb61a275cae80dd5da22f90d884e
lukeknight16112006-1611 <lukeknight16112006@gmail.com>
2026-09-23 14:26:35 +0530

    Basic C

--- basic.c
+++ basic.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main()
-{
+{   
+    //variable declaration
     int age;
     float height;
     char initial;
     char name[100];
 
+    //IO statements
     printf("Enter age: ");
     scanf("%d", &age);
 
@@ -19,11 +21,26 @@ int main()
     printf("Enter name: ");
     scanf("%99s", name);
 
+
+    //different data types
     printf("\nAge: %d\n", age);
     printf("Height: %.3f\n", height);
     printf("Initial: %c\n", initial);
     printf("Name: %s\n", name);
     printf("Hi and Hello World");
+
+//Decison making loop  and operators (unary and logical)
+    if (age>=18 && age<=60){
+        printf("valid age\n");
+    }
+    else {
+        printf("Not valid");
+    }
+    age++;
+    printf("%d\n",age);
+    age--;
+    printf("%d\n",age);
+
 
     return 0;
 }
